711a936532f138eaf77f1c936d7db08e0bf1f8d4
abhinavashish1710 <abhinavashish1106@gmail.com>
2026-07-05 20:50:56 +0530

    Add project files.

---
+++ Assignment01_Drone/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Assignment01_Drone
+{
+    class Drone
+    {
+        private int batteryLife;
+        private int altitude;
+        private bool isFlying;
+
+        public Drone(int battery)
+        {
+            batteryLife = battery;
+            altitude = 0;
+            isFlying = false;
+        }
+
+        public void Fly()
+        {
+            if (batteryLife > 20)
+            {
+                isFlying = true;
+                altitude = 100;
+                Console.WriteLine("Drone is flying.");
+            }
+            else
+            {
+                Console.WriteLine("Battery too low to fly.");
+            }
+        }
+
+        public void Move()
+        {
+            if (isFlying)
+            {
+                Console.WriteLine("Drone is moving.");
+                batteryLife -= 10;
+            }
+            else
+            {
+                Console.WriteLine("Drone cannot move because it is not flying.");
+            }
+        }
+
+        public void Land()
+        {
+            if (isFlying)
+            {
+                isFlying = false;
+                altitude = 0;
+                Console.WriteLine("Drone has landed.");
+            }
+            else
+            {
+                Console.WriteLine("Drone is already on the ground.");
+            }
+        }
+
+        public void ShowBattery()
+        {
+            Console.WriteLine("Battery Life: " + batteryLife + "%");
+        }
+
+        public void ShowAltitude()
+        {
+            Console.WriteLine("Altitude: " + altitude + " meters");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Drone drone = new Drone(80);
+
+            drone.Move();
+
+            drone.Fly();
+
+            drone.Move();
+
+            drone.ShowBattery();
+
+            drone.ShowAltitude();
+
+            drone.Land();
+
+            drone.ShowAltitude();
+        }
+    }
+}
